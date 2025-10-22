694cd8743bec057b787f4a3a11b2af33990ecc13
Ardian Pradana <ardianysp18@gmail.com>
2025-10-22 19:59:25 +0700

    Fix github workflow tool

--- build/larris-dark-mode/index.asset.php
+++ build/larris-dark-mode/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e66c25a75ad778540b32');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ba0674b53d3c11442e4b');
